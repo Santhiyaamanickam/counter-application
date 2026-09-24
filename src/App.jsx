4fb9703f5dae582426d4dec0c9669859108ceb0b
Santhiyaamanickam <santhiyamanickam0703@gmail.com>
2026-09-24 10:43:36 +0530

    Counter Application

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import "./App.css";
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div className="counter">
+      <h1>Counter Application</h1>
+
+      <h2>Count: {count}</h2>
+
+      {count === 0 && <p>Minimum limit reached</p>}
+
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
+
+export default App;
